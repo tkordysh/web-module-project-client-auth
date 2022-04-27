2cd0814b40d488eaf33eb634186ffe384ca770ad
Tony Kordysh <tkordysh@gmail.com>
2022-04-27 06:51:40 -0500

    built out friends list component

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:9000/api/friends", {
+        headers: {
+          authorization: token,
+        },
+      })
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <h2>FriendsList</h2>
       <ul>
-          <li>Name - age - email</li>
-          <li>Name - age - email</li>
-          <li>Name - age - email</li>
-          <li>Name - age - email</li>
+        {friends.map((friend) => {
+          return (
+            <li key={friend.id}>
+              {friend.name} - {friend.age} - {friend.email}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
